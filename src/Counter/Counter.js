9cddb4a14b9b5ff4fc9817bd8e948763330e0fa2
Travis Davis <travisdavismedia@gmail.com>
2022-07-21 12:47:29 -0500

    Counter Example

---
+++ src/Counter/Counter.js
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+function Counter(props){
+    const { startVal, decVal=1, incVal=1} = props;
+    const [  count, setCount] = useState(startVal);
+    function onClick(){
+        setCount(count + incVal);
+    }
+    function onDec(){
+        setCount(count - decVal);
+    }
+    function onReset(){
+        setCount(0);
+    }
+    return  (
+        <>
+            {count}
+            <button onClick={onClick}>i++</button>
+            <button onClick={onDec}>i--</button>
+            <button onClick={onReset}>Reset</button>
+        </>
+    ) ;
+}
+export default Counter;
