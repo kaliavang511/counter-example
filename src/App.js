715823cb07fd2ed7b363cb1df0a75635f7fb439d
Travis Davis <travisdavismedia@gmail.com>
2022-07-21 14:20:33 -0500

    add logout basic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import { useState } from "react";
-import Counter from "./Counter/Counter"
+import Counter from "./componets/Counter/Counter"
+import Login from "./componets/Login/Login"
 function App() {
- return [
-     <Counter startVal={5}/>,
-    <Counter startVal={6}/>,
-    <Counter startVal={7} decVal={2} incVal={2}/>,
- ]
+    let [  loggedin, setLogin]  = useState(false);
+    function onSubmit(username, password){
+        return (event) =>{
+            event.preventDefault();
+            if (username == "user" && password == "pass") {
+               setLogin(true);
+            }
+            console.log("LOGIN SUBMIT", username, password);
+        }
+
+    }
+    function onLogout(){
+        setLogin(false);
+    }
+    let content = null;
+    if (loggedin) {
+        content = (
+            <>
+            <button onClick={onLogout}>Logout</button>
+            <Counter startVal={0}></Counter>
+            </>
+        );
+    } else {
+        content = <Login onSubmit={onSubmit}></Login>
+    }
+    return <>
+     {content}
+    </>
 
 }
 
